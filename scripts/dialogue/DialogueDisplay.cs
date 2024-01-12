e57b6662323e5b5bd521e49c51191e5d7c1cc8c9
Emmett <emmettirl@gmail.com>
2024-01-12 12:35:04 +0000

    Loading multiple lines into ui

--- scripts/dialogue/DialogueDisplay.cs
+++ scripts/dialogue/DialogueDisplay.cs
@@ -8,32 +8,76 @@ public partial class DialogueDisplay : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		String charactersXmlPath = "res://assets/writing/dialogue/characters.xml";
-		String locationsXmlPath = "res://assets/writing/dialogue/locations.xml";
-		String dialogueXmlPath = "res://assets/writing/dialogue/dialogue_example.xml";
+		RunDialogue("dialogue_example.xml");
 		
-		CharacterModel[] characterList = DialogueXmlParser.ParseCharactersFromXml(charactersXmlPath);
-		LocationModel[] locList = DialogueXmlParser.ParseLocationsFromXml(locationsXmlPath);
-		ShotModel[] shotList = DialogueXmlParser.ParseShotsFromXml(dialogueXmlPath);
-		
-		GD.Print(shotList[0].Lines);
+		// RichTextLabel RTlabel = GetNode<RichTextLabel>("TextureRect/MarginContainer/ScrollContainer/VBoxContainer/GridContainer/RichTextLabel");
+		// if (RTlabel != null)
+		// {
+		// 	RTlabel.Text = shotList[0].Lines[0].Text;
+		// 	GD.Print(RTlabel.Text);
+		// }
+		// else
+		// {
+		// 	GD.Print("No label found!");
+		// }
+		//
+		// Label label = GetNode<Label>("TextureRect/MarginContainer/ScrollContainer/VBoxContainer/GridContainer/Label");
+		// if (label != null)
+		// {
+		// 	label.Text = shotList[0].Lines[0].CharacterId;
+		// 	GD.Print(label.Text);
+		// }
+		// else
+		// {
+		// 	GD.Print("No label found!");
+		// }
 
-
-		RichTextLabel label = GetNode<RichTextLabel>("TextVBoxContainer/TextureRect/MarginContainer/TextLabel");
-		if (label != null)
-		{
-			label.Text = shotList[0].Lines[0].Text;
-			GD.Print(label.Text);
-		}
-		else
-		{
-			GD.Print("No label found!");
-		}
 	}
 
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void RunDialogue(String dialogueName)
+	{
+		
+		String charactersXmlPath = "res://assets/writing/dialogue/characters.xml";
+		String locationsXmlPath = "res://assets/writing/dialogue/locations.xml";
+		String dialogueXmlPath = "res://assets/writing/dialogue/" + dialogueName;
+		
+		CharacterModel[] characterList = DialogueXmlParser.ParseCharactersFromXml(charactersXmlPath);
+		LocationModel[] locList = DialogueXmlParser.ParseLocationsFromXml(locationsXmlPath);
+		ShotModel[] shotList = DialogueXmlParser.ParseShotsFromXml(dialogueXmlPath);
+		
+		GD.Print(shotList[0].Lines);
+		
+		VBoxContainer dialogueTarget = GetNode<VBoxContainer>("TextureRect/MarginContainer/ScrollContainer/DialogueTarget_VBoxContainer");
+		var scene = GD.Load<PackedScene>("res://scenes/sub/dialogue_item.tscn");
+
+
+		int i = 0;
+		foreach (ShotModel shot in shotList)
+		{
+			var instance = scene.Instantiate();
+
+			int j = 0;
+			foreach (LineModel line in shot.Lines)
+			{
+				Label label = instance.GetNode<Label>("Label");
+				label.Text = shot.Lines[j].CharacterId;
+			
+				RichTextLabel richTextLabel = instance.GetNode<RichTextLabel>("RichTextLabel");
+				richTextLabel.Text = shot.Lines[j].Text;
+			
+				dialogueTarget.AddChild(instance);
+
+				j++;
+			}
+			
+
+			i++;
+		}
 	}
 }
