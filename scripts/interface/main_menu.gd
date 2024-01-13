4ce84e27620d7f75dfefded09552b67fbfd2759d
Emmett <emmettirl@gmail.com>
2024-01-13 10:47:27 +0000

    Added LICENSE to Repo and updated README.md to reflect it. Minor changes to other files, moved xml to more appropriate folders and updated paths in code. Removed unused process functions in main menu.

--- scripts/interface/main_menu.gd
+++ scripts/interface/main_menu.gd
@@ -1,16 +1,5 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_start_game_button_pressed():
 	#get_tree().change_scene_to_file("res://scenes/Playmat.tscn")
 	scene_loader.scene_folder = "scenes/main"
