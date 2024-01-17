03e97f69886064dc03040b86822bd832e11c88b8
Sinéad Walsh <106690753+VonJellybean@users.noreply.github.com>
2024-01-17 19:41:47 +0000

    Reformatted Combat parser files

--- scripts/XmlParsing/CardXmlParser.cs
+++ scripts/XmlParsing/CardXmlParser.cs
@@ -2,66 +2,58 @@ using System;
 using System.Xml;
 using Godot;
 using TeicsoftSpectacleCards.scripts.customresource.Cards;
+using TeicsoftSpectacleCards.scripts.XmlParsing;
 
 namespace TeicsoftSpectacleCards.scripts.customresource;
 
 public static class CardXmlParser {
-	public static CardModel ParseCardsFromXml(string filePath) {
-		using var file = FileAccess.Open(filePath, FileAccess.ModeFlags.Read);
-		string content = file.GetAsText();
+    public static CardModel ParseCardsFromXml(string filePath) {
+        using FileAccess file = FileAccess.Open(filePath, FileAccess.ModeFlags.Read);
+        string content = file.GetAsText();
 
-		XmlDocument doc = new XmlDocument();
-		doc.LoadXml(content);
-
-
-		XmlNode cardNode = doc.SelectSingleNode("card");
-
-		string cardId = cardNode.Attributes["id"].Value;
-		string cardType = cardNode.Attributes["type"].Value;
-		string modifier = cardNode.Attributes["modifier"].Value;
-		string position = cardNode.Attributes["position"].Value;
+        XmlDocument doc = new();
+        doc.LoadXml(content);
 
 
-		if (Enum.TryParse(modifier, out CardModel.ModifierEnum parsedModifier)) { } else {
-			GD.Print("Failed to parse modifier: " + modifier);
-		}
-		
-		if (Enum.TryParse(position, out CardModel.PositionEnum parsedPosition)) { } else {
-			GD.Print("Failed to parse position: " + position);
-		}
+        XmlNode cardNode = doc.SelectSingleNode("card");
+
+        string cardId = cardNode.Attributes["id"].Value;
+        string cardType = cardNode.Attributes["type"].Value;
+        string modifier = cardNode.Attributes["modifier"].Value;
+        string position = cardNode.Attributes["position"].Value;
 
 
-		XmlNode statsNode = cardNode.SelectSingleNode("stats");
-		int attack = ParseIntNode(statsNode, "attack");
-		int defenseLower = ParseIntNode(statsNode, "defense_lower");
-		int defenseUpper = ParseIntNode(statsNode, "defense_upper");
-		int health = ParseIntNode(statsNode, "health");
-		int draw = ParseIntNode(statsNode, "draw");
-		int discard = ParseIntNode(statsNode, "discard");
-		int spectaclePoints = ParseIntNode(statsNode, "spectacle_points");
+        if (!Enum.TryParse(modifier, out CardModel.ModifierEnum parsedModifier)) {
+            GD.Print("Failed to parse modifier: " + modifier);
+        }
 
-		XmlNode designNode = cardNode.SelectSingleNode("design");
-		string imagePath = ParseTextNode(designNode, "image");
-		string animationPath = ParseTextNode(designNode, "animation");
-		string soundPath = ParseTextNode(designNode, "sound");
+        if (!Enum.TryParse(position, out CardModel.PositionEnum parsedPosition)) {
+            GD.Print("Failed to parse position: " + position);
+        }
 
-		XmlNode textNode = cardNode.SelectSingleNode("text");
-		string name = ParseTextNode(textNode, "name");
-		string description = ParseTextNode(textNode, "description");
-		string lore = ParseTextNode(textNode, "lore");
-		string tooltip = ParseTextNode(textNode, "tooltip_text");
 
-		CardModel card = CardFactory.MakeCard(cardType, cardId, parsedModifier, parsedPosition, attack, defenseLower, defenseUpper,
-			health, draw, discard, spectaclePoints, name, description, lore, tooltip, imagePath, animationPath, soundPath);
+        XmlNode statsNode = cardNode.SelectSingleNode("stats");
+        int attack = Utils.ParseIntNode(statsNode, "attack");
+        int defenseLower = Utils.ParseIntNode(statsNode, "defense_lower");
+        int defenseUpper = Utils.ParseIntNode(statsNode, "defense_upper");
+        int health = Utils.ParseIntNode(statsNode, "health");
+        int draw = Utils.ParseIntNode(statsNode, "draw");
+        int discard = Utils.ParseIntNode(statsNode, "discard");
+        int spectaclePoints = Utils.ParseIntNode(statsNode, "spectacle_points");
 
-		return card;
+        XmlNode designNode = cardNode.SelectSingleNode("design");
+        string imagePath = Utils.ParseTextNode(designNode, "image");
+        string animationPath = Utils.ParseTextNode(designNode, "animation");
+        string soundPath = Utils.ParseTextNode(designNode, "sound");
 
-		int ParseIntNode(XmlNode parent, string s) {
-			return parent.SelectSingleNode(s) != null ? Int32.Parse(parent.SelectSingleNode(s).InnerText) : 0;
-		}
+        XmlNode textNode = cardNode.SelectSingleNode("text");
+        string name = Utils.ParseTextNode(textNode, "name");
+        string description = Utils.ParseTextNode(textNode, "description");
+        string lore = Utils.ParseTextNode(textNode, "lore");
+        string tooltip = Utils.ParseTextNode(textNode, "tooltip_text");
 
-		string ParseTextNode(XmlNode parent, string s) {
-			return parent.SelectSingleNode(s) != null ? parent.SelectSingleNode(s).InnerText : "";
-		}
-	}
+        return CardFactory.MakeCard(cardType, cardId, parsedModifier, parsedPosition, attack, defenseLower,
+            defenseUpper, health, draw, discard, spectaclePoints, name, description, lore, tooltip, imagePath,
+            animationPath, soundPath);
+    }
 }
