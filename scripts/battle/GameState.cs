4220df32291fc3283ec99b96bc53387e701d090a
Emmett <emmettirl@gmail.com>
2024-01-18 23:08:08 +0000

    EOD commit

--- scripts/battle/GameState.cs
+++ scripts/battle/GameState.cs
@@ -14,22 +14,24 @@ public class GameState
     public int PlayerHealth { get; set; }
 
     public List<string> CardStack { get; set; } 
-    
-    
     private List<ComboModel> Combos { get; set; }
 
-    //Constructor
+    // Constructor
     public GameState()
     {
         this.Combos = ParseAllCombos();
 
         CardStack = new List<string>();
 
+        ComboMultiplier = 1;
         ComboCompare();
-    }
+        SpectaclePoints = 0;
 
+        MaxPlayerHealth = 100; // adjust this as needed, or base on some other check
+        PlayerHealth = MaxPlayerHealth;
+    }
     
-    //Add card ID to "Stack"
+    // Stack Methods
     public void PushCardStack(string id)
     {
         this.CardStack.Add(id);
@@ -38,38 +40,75 @@ public class GameState
     public void CleanCardStack()
     {
         this.CardStack = new List<string>();
+
+    }
+    
+    public bool IsPlayerAlive()
+    {
+        return PlayerHealth > 0;
     }
 
+    public void AdjustPlayerHealth(int amount)
+    {
+        PlayerHealth += amount;
 
-    public bool ComboCompare()
+        if (PlayerHealth > MaxPlayerHealth)
+        {
+            PlayerHealth = MaxPlayerHealth;
+        }
+        
+        if (!IsPlayerAlive())
+        {
+            //end round on loss
+        }
+    }
+    
+    
+
+    public void ComboCalulation(int comboAdjustmentAmount)
+    {
+        // Placeholder for combo math
+        
+        if (comboAdjustmentAmount <= 1)
+        {
+            comboAdjustmentAmount = 1;
+        }
+        
+        this.ComboMultiplier += comboAdjustmentAmount;
+    }
+
+    public int BlunderCount(ComboModel comboModel, List<string> CardStack)
+    {
+        return CardStack.Count - comboModel.CardList.Count;
+    }
+
+    
+    // Check for combo matches
+    public ComboModel ComboCompare()
     {
 
-        foreach (ComboModel Combo in Combos)
+        foreach (ComboModel combo in Combos)
         {
-            int i = Combo.CardList.Count -1;
+            int i = combo.CardList.Count -1;
 
             bool match = true;
             while (i>=0)
             {
-                if (CardStack[-i] != Combo.CardList[-i].Id)
+                if (CardStack[-i] != combo.CardList[-i].Id)
                 {
                     match = false;
                     break;
                 }
                 i--;
             }
-            
-            if (match)
-            {
-                return true;
-            }
+            if (match) { return combo; }
         }
 
-        return false;
+        return null;
     }
     
     
-    //Retrieve a list of all combos as model objects
+    // Retrieve a list of all combos as model objects
     public List<ComboModel> ParseAllCombos()
     {
         string comboFilePath = "res://data/combos/";
