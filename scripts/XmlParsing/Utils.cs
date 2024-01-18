a292aa5db00656f94080d9105e6e0b75bf2d453b
Emmett <emmettirl@gmail.com>
2024-01-18 16:37:26 +0000

    updated Battle.cs to have 4space indentation

--- scripts/XmlParsing/Utils.cs
+++ scripts/XmlParsing/Utils.cs
@@ -3,14 +3,15 @@ using System.Xml;
 
 namespace TeicsoftSpectacleCards.scripts.XmlParsing;
 
-public static class Utils {
-
-    public static int ParseIntNode(XmlNode parent, string s) {
+public static class Utils
+{
+    public static int ParseIntNode(XmlNode parent, string s)
+    {
         return parent.SelectSingleNode(s) != null ? int.Parse(parent.SelectSingleNode(s).InnerText) : 0;
     }
 
-    public static string ParseTextNode(XmlNode parent, string s) {
+    public static string ParseTextNode(XmlNode parent, string s)
+    {
         return parent.SelectSingleNode(s) != null ? parent.SelectSingleNode(s).InnerText : "";
     }
-
 }
--- scripts/battle/Battle.cs
+++ scripts/battle/Battle.cs
@@ -7,107 +7,120 @@ using TeicsoftSpectacleCards.scripts.customresource;
 using TeicsoftSpectacleCards.scripts.XmlParsing;
 using TeicsoftSpectacleCards.scripts.XmlParsing.models;
 
-public partial class Battle : Node2D {
+public partial class Battle : Node2D
+{
+    private static readonly List<Color> COLORS = new()
+    {
+        new(1.0f, 0.0f, 0.0f),
+        new(1.0f, 0.0f, 0.0f),
+        new(1.0f, 0.0f, 0.0f),
+        new(1.0f, 0.0f, 0.0f),
+        new(0.0f, 1.0f, 0.0f),
+        new(0.0f, 1.0f, 0.0f),
+        new(0.0f, 0.0f, 1.0f),
+        new(0.0f, 0.0f, 1.0f),
+    };
 
-	private static readonly List<Color> COLORS = new() {
-		new(1.0f, 0.0f, 0.0f),
-		new(1.0f, 0.0f, 0.0f),
-		new(1.0f, 0.0f, 0.0f),
-		new(1.0f, 0.0f, 0.0f),
-		new(0.0f, 1.0f, 0.0f),
-		new(0.0f, 1.0f, 0.0f),
-		new(0.0f, 0.0f, 1.0f),
-		new(0.0f, 0.0f, 1.0f),
-	};
+    private static readonly Color[] PALETTE =
+    {
+        new(1.0f, 0.0f, 0.0f),
+        new(1.0f, 1.0f, 0.0f),
+        new(0.0f, 1.0f, 0.0f),
+        new(0.0f, 1.0f, 1.0f),
+        new(0.0f, 0.0f, 1.0f),
+        new(1.0f, 0.0f, 1.0f),
+        new(1.0f, 1.0f, 1.0f),
+        new(0.0f, 0.0f, 0.0f),
+    };
 
-	private static readonly Color[] PALETTE = {
-		new(1.0f, 0.0f, 0.0f),
-		new(1.0f, 1.0f, 0.0f),
-		new(0.0f, 1.0f, 0.0f),
-		new(0.0f, 1.0f, 1.0f),
-		new(0.0f, 0.0f, 1.0f),
-		new(1.0f, 0.0f, 1.0f),
-		new(1.0f, 1.0f, 1.0f),
-		new(0.0f, 0.0f, 0.0f),
-	};
+    [Export] private PackedScene cardScene;
+    [Export] private PackedScene enemyScene;
+    private Hand hand;
+    private Deck deck;
+    private Discard discard;
+    private PathFollow2D enemiesLocation;
+    private List<Enemy> enemies = new();
+    private int selectedEnemyIndex = -1;
 
-	[Export] private PackedScene cardScene;
-	[Export] private PackedScene enemyScene;
-	private Hand hand;
-	private Deck deck;
-	private Discard discard;
-	private PathFollow2D enemiesLocation;
-	private List<Enemy> enemies = new();
-	private int selectedEnemyIndex = -1;
+    public override void _Ready()
+    {
+        //This is a test to see if the card factory works, feel free to remove it
+        Card modelCard = CardXmlParser.ParseCardsFromXml("res://data/cards/card_template.xml");
+        GD.Print(modelCard + "\n");
 
-	public override void _Ready() {
-		//This is a test to see if the card factory works, feel free to remove it
-		Card modelCard = CardXmlParser.ParseCardsFromXml("res://data/cards/card_template.xml");
-		GD.Print(modelCard + "\n");
+        //This is a test to see if the combo parsing works, feel free to remove it
+        ComboModel combo = ComboXmlParser.ParseComboFromXml("res://data/combos/combo_template.xml");
+        GD.Print(combo + "\n");
 
-		//This is a test to see if the combo parsing works, feel free to remove it
-		ComboModel combo = ComboXmlParser.ParseComboFromXml("res://data/combos/combo_template.xml");
-		GD.Print(combo + "\n");
+        // //This is a test to see if the Deck parsing works, feel free to remove it
+        DeckModel deckModel = DeckXmlParser.ParseDeckFromXml("res://data/decks/deck_template.xml");
+        GD.Print(deckModel + "\n");
+        hand = GetNode<Hand>("Hand");
+        deck = new Deck();
+        discard = new Discard();
+        enemiesLocation = GetNode<PathFollow2D>("Enemies/EnemiesLocation");
+        hand.discard = discard;
+        deck.discard = discard;
+        List<Card> initialDeck = new();
+        foreach (Color color in COLORS)
+        {
+            Card card = cardScene.Instantiate<Card>();
+            card.ChangeColor(color);
+            initialDeck.Add(card);
+        }
 
-		// //This is a test to see if the Deck parsing works, feel free to remove it
-		DeckModel deckModel = DeckXmlParser.ParseDeckFromXml("res://data/decks/deck_template.xml");
-		GD.Print(deckModel + "\n");
-		hand = GetNode<Hand>("Hand");
-		deck = new Deck();
-		discard = new Discard();
-		enemiesLocation = GetNode<PathFollow2D>("Enemies/EnemiesLocation");
-		hand.discard = discard;
-		deck.discard = discard;
-		List<Card> initialDeck = new();
-		foreach (Color color in COLORS) {
-			Card card = cardScene.Instantiate<Card>();
-			card.ChangeColor(color);
-			initialDeck.Add(card);
-		}
+        Card lastCard = cardScene.Instantiate<Card>();
+        lastCard.ChangeColor(PALETTE[GD.Randi() % PALETTE.Length]);
+        initialDeck.Add(lastCard);
 
-		Card lastCard = cardScene.Instantiate<Card>();
-		lastCard.ChangeColor(PALETTE[GD.Randi() % PALETTE.Length]);
-		initialDeck.Add(lastCard);
-
-		deck.AddCards(initialDeck);
-		deck.Shuffle();
+        deck.AddCards(initialDeck);
+        deck.Shuffle();
 
 
-		float locationRatio = 1f / 2;
-		foreach (int i in Enumerable.Range(0, 3)) {
-			Enemy enemy = enemyScene.Instantiate<Enemy>();
-			enemy.EnemySelected += SelectEnemy;
-			enemies.Add(enemy);
-			enemiesLocation.ProgressRatio = i * locationRatio;
-			enemy.Position = enemiesLocation.Position;
-			AddChild(enemy);
-		}
-	}
+        float locationRatio = 1f / 2;
+        foreach (int i in Enumerable.Range(0, 3))
+        {
+            Enemy enemy = enemyScene.Instantiate<Enemy>();
+            enemy.EnemySelected += SelectEnemy;
+            enemies.Add(enemy);
+            enemiesLocation.ProgressRatio = i * locationRatio;
+            enemy.Position = enemiesLocation.Position;
+            AddChild(enemy);
+        }
+    }
 
-	public override void _Process(double delta) { }
+    public override void _Process(double delta)
+    {
+    }
 
-	public void PlaySelectedCard() {
-		Card card = hand.GetSelectedCard();
-		if (card != null && !(card.RequiresTarget() && GetSelectedEnemy() == null)) {
-			card.Play(GetSelectedEnemy(), enemies);
-			hand.DiscardSelectedCard();
-		}
-	}
+    public void PlaySelectedCard()
+    {
+        Card card = hand.GetSelectedCard();
+        if (card != null && !(card.RequiresTarget() && GetSelectedEnemy() == null))
+        {
+            card.Play(GetSelectedEnemy(), enemies);
+            hand.DiscardSelectedCard();
+        }
+    }
 
-	private Enemy GetSelectedEnemy() {
-		return selectedEnemyIndex != -1 ? enemies[selectedEnemyIndex] : null;
-	}
+    private Enemy GetSelectedEnemy()
+    {
+        return selectedEnemyIndex != -1 ? enemies[selectedEnemyIndex] : null;
+    }
 
-	private void SelectEnemy(Enemy enemy) {
-		int enemyIndex = enemies.IndexOf(enemy);
-		selectedEnemyIndex = selectedEnemyIndex != enemyIndex ? enemyIndex : -1;
-	}
+    private void SelectEnemy(Enemy enemy)
+    {
+        int enemyIndex = enemies.IndexOf(enemy);
+        selectedEnemyIndex = selectedEnemyIndex != enemyIndex ? enemyIndex : -1;
+    }
 
-	private void OnPlayButtonPressed() {
-		PlaySelectedCard();
-	}
+    private void OnPlayButtonPressed()
+    {
+        PlaySelectedCard();
+    }
 
-	private void OnDeckPressed() {
-		hand.AddCards(deck.DrawCard());
-	}
+    private void OnDeckPressed()
+    {
+        hand.AddCards(deck.DrawCard());
+    }
 }
