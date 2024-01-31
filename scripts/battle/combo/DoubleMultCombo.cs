153a9f003d76e13c8828f5be4e4e5b1bcbdc46b3
Sinéad Walsh <106690753+VonJellybean@users.noreply.github.com>
2024-01-31 21:14:34 +0000

    Added EtTuBrutal combo

--- scripts/battle/combo/DoubleMultCombo.cs
+++ scripts/battle/combo/DoubleMultCombo.cs
@@ -7,7 +7,6 @@ namespace TeicsoftSpectacleCards.scripts.battle;
 public class DoubleMultCombo : Combo {
 
     public override void Play(GameState gameState) {
-        GD.Print("MULTIPLIER DOUBLED!");
         gameState.Multiplier *= 2;
         base.Play(gameState);
     }
---
+++ scripts/battle/combo/SelfDamageCombo.cs
@@ -0,0 +1,10 @@
+﻿using TeicsoftSpectacleCards.scripts.battle.target;
+
+namespace TeicsoftSpectacleCards.scripts.battle;
+
+public class SelfDamageCombo:Combo {
+    public override void Play(GameState gameState) {
+        gameState.Player.DirectDamage(5);
+        base.Play(gameState);
+    }
+}
