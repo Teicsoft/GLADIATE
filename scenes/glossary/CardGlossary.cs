810ad86c293e84986bce457abfcccde584ddc746
Emmett <emmett.fitzharris@mycit.ie>
2024-02-22 15:12:49 +0000

    Card Glossary data populating correctly

--- scenes/glossary/CardGlossary.cs
+++ scenes/glossary/CardGlossary.cs
@@ -18,12 +18,27 @@ public partial class CardGlossary : Control
             Node packedScene = ResourceLoader.Load<PackedScene>("res://scenes/glossary/card_glossary_item.tscn").Instantiate();
             packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/CardNameMargin/CardName").Text = card.CardName;
             packedScene.GetNode<TextureRect>("VBoxContainer/ImageMargin/CardImage").Texture = (Texture2D)GD.Load(card.ImagePath);
-            packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer/VBoxContainer/Attack").Text = card.Attack.ToString();
-            packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer2/VBoxContainer/DefL").Text = card.DefenseLower.ToString();
-            packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer3/VBoxContainer/DefH").Text = card.DefenseUpper.ToString();
-            packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer4/VBoxContainer/Heal").Text = card.Health.ToString();
-            packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer5/VBoxContainer/Draw").Text = card.CardDraw.ToString();
-            packedScene.GetNode<Label>("VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer6/VBoxContainer/Discard").Text = card.Discard.ToString();
+            
+            // Stats
+            string statsPathPrefix = "VBoxContainer/ContentMargin/VBoxContainer/StatsGridItemMargin/HBoxContainer/MarginContainer";
+            packedScene.GetNode<Label>(statsPathPrefix + "1/VBoxContainer/Attack").Text = card.Attack.ToString();
+            packedScene.GetNode<Label>(statsPathPrefix + "2/VBoxContainer/DefL").Text = card.DefenseLower.ToString();
+            packedScene.GetNode<Label>(statsPathPrefix+ "3/VBoxContainer/DefH").Text = card.DefenseUpper.ToString();
+            packedScene.GetNode<Label>(statsPathPrefix + "4/VBoxContainer/Heal").Text = card.Health.ToString();
+            packedScene.GetNode<Label>(statsPathPrefix + "5/VBoxContainer/Draw").Text = card.CardDraw.ToString();
+            packedScene.GetNode<Label>(statsPathPrefix + "6/VBoxContainer/Discard").Text = card.Discard.ToString();
+            packedScene.GetNode<Label>(statsPathPrefix + "7/VBoxContainer/Spectacle").Text = card.SpectaclePoints.ToString();
+
+            // text
+            packedScene.GetNode<Label>(
+                    "VBoxContainer/ContentMargin/VBoxContainer/DescriptionGridItemMargin2/VBoxContainer/MarginContainer/VBoxContainer/Description")
+                .Text = card.Description;
+            packedScene.GetNode<Label>(
+                    "VBoxContainer/ContentMargin/VBoxContainer/DescriptionGridItemMargin2/VBoxContainer/MarginContainer2/VBoxContainer/Lore")
+                .Text = card.Lore;
+
+
+
             VBoxContainer.AddChild(packedScene);
         }
     }
