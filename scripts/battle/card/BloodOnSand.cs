f54db76dee399363ec6fde6921fbeaeedbe55604
Sinéad Walsh <106690753+VonJellybean@users.noreply.github.com>
2024-05-20 21:46:43 +0100

    Update BloodOnSand.cs

--- scripts/battle/card/BloodOnSand.cs
+++ scripts/battle/card/BloodOnSand.cs
@@ -5,8 +5,9 @@ namespace GLADIATE.scripts.battle.card;
 
 public class BloodOnSand : Card {
     public override void Play(GameState gameState, ITarget target, ITarget player) {
-        player.DirectDamage(6);
+        int selfDamage = 6;
+        player.DirectDamage(selfDamage);
         base.Play(gameState, target, player);
-        GD.Print(" * " + "Damaging Self for 6");
+        GD.Print(" * " + "Damaging Self for " + selfDamage);
     }
 }
